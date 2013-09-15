da01e06443a0f1cfc015d69dc8101b323b2183f0
tushartonmoy <tushartonmoy@gmail.com>
2013-09-16 01:59:59 +0530

    commit -m "Uploading files..."

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Md Rezwan Al Islam - Your Personal Consultant</title>
+        <link href="css/template.css" rel="stylesheet" type="text/css" />
+        <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" /> 
+        
+    </head>
+
+    <body>
+        <div id="maincontent">
+
+            <div class="contentcontainer">
+                <div class="row">
+                    <div class="col contentheading">Md Rezwan Al Islam</div>
+                </div>
+                <div class="row">
+                    <div class="col placeholder"><img src="images/about_me.jpg" alt=" " height="130px" /></div>
+                    <div class="col contenttext">
+                   	  <div class="heading">About</div>
+                        <p>Rezwan is a Summa Cum Laude graduate of the class of 2012, School of Communication at Northwestern University in Qatar. He holds a degree in Communication, and is currently pursing a Masters in Digital Media Arts and Instructional Design from Harvard University. </p>
+                        <p>&nbsp;</p>
+                        
+                        <font size="4">Digital Strategist		 |				 Business Expert</font>
+                        <p>&nbsp;</p>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div id="servicescontainer">
+                <div class="row">
+                    <div class="col servicescontainerheading">Work</div><p>&nbsp;</p><p>He currently serves as the Director of New Media at Fuego Digital Media QSTP. Fuego Digital Media QSTP is Qatar's premier web, web based systems, technology and new media firm. His primary responsibilities include devising online communication strategy for clients, innovating within social, mobile, and digital media and technologies, creating new content delivery channels and platforms, business development as well as project management.<br />
+                      <br />
+                      Prior to Fuego, Rezwan has worked for QF Radio, The Pearl Qatar, and Al Jazeera Arabic Satellite Channel. Rezwan is also involved in multiple startups, and spends a lot of time consulting for various projects of his interests, mainly in the digital industry.</p>
+                </div>
+                <div class="row"><p>&nbsp;</p>
+                    <div class="col servicescontainerheading">Code Editor</div>
+                    <p>&nbsp;</p>
+Adobe Dreamweaver CC.</div>
+                </div>
+            </div>
+        </div>
+	
+		<div id="footer">
+            <div id="footercontainer">
+                <div class="follow">
+
+                    <p class="followtext">Follow Rezwan</p>
+                    <ul>
+                        <li>
+                            <a href="https://twitter.com/rezwanai"><img src="images/twitter.jpg" alt="Rezwan's Twitter Handle" /></a>
+                        </li>
+                        <li>
+                            <a href="http://www.facebook.com/tushartonmoy"><img src="images/facebook.jpg" alt="Rezwan's Facebook Page" /></a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="privacycontainer">rezwanai.com @2013</div>
+            </div>
+        </div>
+        
+    </body>
+</html>
